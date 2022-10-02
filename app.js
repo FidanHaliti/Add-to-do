4cc51355499d82fac128048edf9239ec7509015f
Fidan Haliti <fidan91haliti@gmail.com>
2022-10-02 17:16:46 +0200

    first commit

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-let Teksti = document.getElementById("inputText");
-let Shto = document.getElementById("AddToDo");
-let Fshije = document.getElementById("ClearToDo");
-let Kontinieri = document.getElementById("toDoContainer");
+let violeta = document.getElementById('AddToDo');
+let fidan = document.getElementById('ClearToDo');
+let asja = document.getElementById('inputText');
+let skender = document.getElementById('toDoContainer');
 
-Shto.addEventListener("click", function () {
-  let paragraf = document.createElement("p");
-  paragraf.classList.add("paragrafi");
-  Kontinieri.appendChild(paragraf);
-  paragraf.innerHTML = Teksti.value;
-  Teksti.value = "";
+violeta.addEventListener('click', function(){
+let paragraf = document.createElement('p');
+skender.appendChild(paragraf);
+paragraf.innerHTML = asja.value;
+asja.value =""
+paragraf.classList.add('paragrafi');
 
-  paragraf.addEventListener("click", function () {
-    paragraf.style.textDecoration = "line-through";
-  });
+paragraf.addEventListener('click', function() {
+    paragraf.style.textDecoration = 'line-through';
+})
+paragraf.addEventListener('dblclick', function() {
+skender.removeChild(paragraf);
+})
 
-  paragraf.addEventListener("dblclick", function () {
-    Kontinieri.removeChild(paragraf);
-  });
-  Fshije.addEventListener("click", function () {
-    Kontinieri.removeChild(paragraf);
-  });
-});
+fidan.addEventListener('click', function() {
+    skender.removeChild(paragraf); 
+})
+})
+
+
