525daf4876c03be1e74b59a50caa35c165e76fde
Fidan Haliti <fidan91haliti@gmail.com>
2022-10-04 14:10:01 +0200

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+const Text = document.getElementById("inputText");
+const addToButton = document.getElementById("AddToDo");
+let ClearToDo = document.getElementById("ClearToDo");
+let ToDos = document.getElementById("toDoContainer");
+
+addToButton.addEventListener("click", function () {
+  let paragraph = document.createElement("p");
+  paragraph.classList.add("paragrafi");
+  ToDos.appendChild(paragraph);
+  paragraph.innerHTML = Text.value;
+  Text.value = "";
+
+  paragraph.addEventListener("click", function () {
+    paragraph.style.textDecoration = "line-through";
+  });
+
+  paragraph.addEventListener("dblclick", function () {
+    ToDos.removeChild(paragraph);
+  });
+
+  ClearToDo.addEventListener("click", function () {
+    ToDos.removeChild(paragraph);
+  });
+  var list = document.querySelector(".to-dos");
+new Sortable(list,{ animation:500})
+});
 
 
 
