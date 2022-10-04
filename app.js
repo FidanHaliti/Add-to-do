06ff815b06f696c6954b51938cdd98db820e1cb1
Fidan Haliti <fidan91haliti@gmail.com>
2022-10-04 14:04:45 +0200

    first commit

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-let violeta = document.getElementById('AddToDo');
-let fidan = document.getElementById('ClearToDo');
-let asja = document.getElementById('inputText');
-let skender = document.getElementById('toDoContainer');
 
-violeta.addEventListener('click', function(){
-let paragraf = document.createElement('p');
-skender.appendChild(paragraf);
-paragraf.innerHTML = asja.value;
-asja.value =""
-paragraf.classList.add('paragrafi');
-
-paragraf.addEventListener('click', function() {
-    paragraf.style.textDecoration = 'line-through';
-})
-paragraf.addEventListener('dblclick', function() {
-skender.removeChild(paragraf);
-})
-
-fidan.addEventListener('click', function() {
-    skender.removeChild(paragraf); 
-})
-})
 
 
